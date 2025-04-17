7e70dec5f40c018a46997396d9da72a87a9471a8
unknown <sandiejgo@gmail.com>
2025-04-17 21:40:36 +0300

    Added data set

---
+++ test.php
@@ -0,0 +1,61 @@
+<?php
+$data = [
+    [
+        "internal_history_id" => "2230893",
+        "external_id" => "8615",
+        "external_commission" => "0.0005"
+    ],
+    [
+        "internal_history_id" => "2230891",
+        "external_id" => "2159",
+        "external_commission" => "0.0200"
+    ],
+    [
+        "internal_history_id" => "2230892",
+        "external_id" => "5349",
+        "external_commission" => "0.0060"
+    ],
+    [
+        "internal_history_id" => "563089",
+        "external_id" => "8659",
+        "external_commission" => "0.0054"
+    ],
+    [
+        "internal_history_id" => "217403",
+        "external_id" => "1462",
+        "external_commission" => "0.0040"
+    ],
+    [
+        "internal_history_id" => "2230883",
+        "external_id" => "8859",
+        "external_commission" => "0.0008"
+    ],
+    [
+        "internal_history_id" => "2230581",
+        "external_id" => "5988",
+        "external_commission" => "0.0040"
+    ],
+    [
+        "internal_history_id" => "2230592",
+        "external_id" => "8659",
+        "external_commission" => "0.9000"
+    ],
+    [
+        "internal_history_id" => "208998",
+        "external_id" => "8599",
+        "external_commission" => "0.0600"
+    ],
+    [
+        "internal_history_id" => "217467",
+        "external_id" => "7612",
+        "external_commission" => "0.0090"
+    ],
+    [
+        "internal_history_id" => "2308935",
+        "external_id" => "6923",
+        "external_commission" => "0.0080"
+    ]
+];
+
+
+?>
