902dfa5aa45d557d4ad5164c0c78a1b944485352
unknown <sandiejgo@gmail.com>
2025-04-17 21:27:31 +0300

    Added new condition

--- test.sql
+++ test.sql
@@ -1,3 +1,3 @@
-SELECT id,ar,epc,epl
+SELECT id,cr,ar,epc,epl
 FROM test
-WHERE ar = (SELECT ar FROM test WHERE id=29)
+WHERE cr = (SELECT cr FROM test WHERE id=29) AND ar = (SELECT ar FROM test WHERE ID=29)
